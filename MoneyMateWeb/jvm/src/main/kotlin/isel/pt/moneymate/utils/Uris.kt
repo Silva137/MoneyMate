4af87ebb99987df794afb0af1ea704900c1fb0e6
joaoRocha47196 <joaocrocha2001@gmail.com>
2023-04-10 23:51:22 +0100

    Uris File Changes

--- MoneyMateWeb/jvm/src/main/kotlin/isel/pt/moneymate/utils/Uris.kt
+++ MoneyMateWeb/jvm/src/main/kotlin/isel/pt/moneymate/utils/Uris.kt
@@ -14,8 +14,8 @@ object Uris {
     object Users {
         const val CREATE = "/users/register"
         const val LOGIN = "/users/login"
-        const val UPDATE = "/users/{userId}"
-        const val DELETE = "/users/{userId}"
+        const val UPDATE = "/users"
+        const val DELETE = "/users"
         const val GET_BY_ID = "/users/{id}"
     }
 
@@ -26,9 +26,9 @@ object Uris {
          * Private - Given a userId and a Name
          * Shared - Given a userId and a Name, after associating otherUsers, userId becomes a "systemUser"
          */
-        const val CREATE = "/wallets/users{userId}"
+        const val CREATE = "/wallets"
 
-        const val GET_ALL_FROM_USER = "/wallets/users{userId}"
+        const val GET_ALL_FROM_USER = "/wallets"
         const val GET_BY_ID = "/wallets/{walletId}"
 
         const val UPDATE_NAME = "/wallets/{walletId}"
@@ -47,7 +47,7 @@ object Uris {
          * For a shared Wallet
          * For a shared Wallet Association (A transaction that represents a Shared Wallet Balance in a Private Wallet)
          */
-        const val CREATE = "/transactions/wallets/{walletId}/users/{userId}"
+        const val CREATE = "/transactions/wallets/{walletId}"
 
         /**
          * Obtain transactions of a wallet (Shared or Private):
@@ -63,7 +63,7 @@ object Uris {
          *
          * Ex: /transactions?walletId={id}&minPrice={min}&maxPrice={max}
          */
-        const val GET_FROM_WALLET_OF_USER = "/transactions/wallets/{walletId}/users/{userId}"
+        const val GET_FROM_WALLET_OF_USER = "/transactions/wallets/{walletId}"
 
         /**
          * Obtain transactions of all the private Wallets of a User:
@@ -71,7 +71,7 @@ object Uris {
          * By price
          * By category
          */
-        const val GET_FROM_ALL_PRIVATE_WALLETS_OF_USER = "/transactions/users/{userId}"
+        const val GET_FROM_ALL_PRIVATE_WALLETS_OF_USER = "/transactions"
 
         const val GET_BY_ID = "/transactions/{transactionId}"
         const val UPDATE = "/transactions/{transactionId}"
@@ -84,12 +84,12 @@ object Uris {
          * System - Created autmatically for all Users
          * Custom - Created expecifically by a User
          */
-        const val CREATE = "/categories/user/{userId}"
+        const val CREATE = "/categories"
 
         /**
          * Get all system Categories (userId = "system") or all cateogries of a User
          */
-        const val GET_CATEGORIES = "/categories/user/{userId}"
+        const val GET_CATEGORIES = "/categories"
 
         const val GET_BY_ID = "/categories/{categoryId}"
         const val UPDATE = "/categories/{categoryId}"
